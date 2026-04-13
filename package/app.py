bc8c55117bf62f4eb4a94c4d59486375334ebdce
Wyao <1827283251@qq.com>
2026-04-13 22:29:26 +0800

    精简了steamlit云端调用时所需的包

--- package/app.py
+++ package/app.py
@@ -7,6 +7,7 @@ its Streamlit UI code. Streamlit will use this file as the app root.
 # Ensure project root is importable
 import os
 import sys
+
 project_root = os.path.dirname(os.path.dirname(__file__))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
